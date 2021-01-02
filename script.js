26240c3068e0da5d044857ad1b1544010a07cac6
rivaldi tri <rivaldi07tri@gmail.com>
2021-01-02 20:47:05 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function myFunction(){
+	var nama = prompt('ketik nama spesial kamu disini');
+	alert('hai ' + nama + ' Jaga kesehatan yaa...');
+	alert('jangan sakit sakitan');
+	alert('aku gamau liat kamu sakit');
+	alert('makan jangan sampe telat');
+	alert('sholatnya juga');
+	var kondisi = confirm('kamu sayang gak si sama aku');
+	if (kondisi) {
+		alert('iya sama aku juga sayang kamu');
+		alert('terakhir coba kamu klik tulisan dibawah tombol pesen buat kamu');
+	}else {
+		alert(':( yaudah gapapa aku tetep sayang kamu kok');
+		alert('coba klik tulisan dibawah tombol pesen buat kamu');
+	}
+}
